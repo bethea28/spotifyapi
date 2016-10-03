9d14148a1730463e9ea4c9a1cfdc8384d3853d69
bryan <bryanbethea28@gmail.com>
2016-10-03 01:49:39 -0400

    succ

--- src/App.js
+++ src/App.js
@@ -1,41 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Router, Route, hashHistory, Link, IndexRoute} from 'react-router';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Link, Route, Router, hashHistory} from 'react-router'
+import navstyle from './components/maincss.js'
+import Song from './components/Song.js'
+import Playlist from './components/Playlist.js'
+import Songsearch from './components/Songsearch.js'
+import $ from 'jquery'
 
-import 'bootstrap/dist/css/bootstrap.css';
-import BrandStory from './components/BrandStory';
-import Footer from './components/Footer';
-import ItemPage from './components/ItemPage';
-import Header from './components/Header';
-import HomePage from './components/HomePage';
-import LookBookPage from './components/LookBookPage';
-import SalePage from './components/SalePage';
-import ShopPage from './components/ShopPage';
-import ToolBar from './components/ToolBar';
-import NavBar from './components/NavBar';
+let App = React.createClass({
+  getIntialState(){
+    return ({song:'', songData: 0})
+  },
 
-var App = React.createClass({
-  render: function() {
+
+
+  handleChange(event){
+  event.preventDefault()
+  var name = event.target.value
+  // console.log(name)
+  // this.finalChange(name)
+    this.setState({song:name})
+ },
+
+
+ finalData(result){
+    this.setState({song:'', songData:result})
+    console.log(result.tracks.items)
+ },
+
+
+  getSong(event){
+    event.preventDefault()
+    // console.log(this.state.song)
+    // var that = this
+    $.ajax({
+      url:'https://api.spotify.com/v1/search?q=' + this.state.song + '&type=track&market=US',
+      success:function print(data){
+        var result = data
+        this.finalData(result)
+        // console.log(result)
+      }.bind(this)
+      
+
+    })
+
+  },
+
+
+  render(){
+
+    // pic
+    // song title
+    // artist
     return (
-      <div className="container-fluid">
-        <Header />
-        {this.props.children}
-        <Footer />
-      </div>
+
+        <div>
+          <form>
+            <input id= 'butt' type="text"  name = 'input'  onChange={this.handleChange}/> 
+            <input type="submit" name='submit'  onClick={this.getSong} /> 
+
+          </form>
+                   
+        </div>
+          
     )
   }
 })
 
+
+
 ReactDOM.render(
-    <Router history={hashHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={HomePage} />
-        <Route path="lookbook" component={LookBookPage} />
-        <Route path="brand" component={BrandStory} />
-        <Route path="sale" component={SalePage} />
-        <Route path="shop" component={ShopPage} />
-        <Route path="item" component={ItemPage} />
-      </Route>
-    </Router>,
+  <Router history={hashHistory}>
+    <Route path='/' component={App}/>
+  </Router>,  
   document.getElementById('root')
-);
+
+)
---
+++ src/components/maincss.js
@@ -0,0 +1,9 @@
+var navstyle ={
+  backgroundColor :'gold',
+  width: 100 ,
+  height: 100,
+  borderRadius:10,
+
+ }
+
+export default navstyle
